d799a8a998a1ab4749c7d8c95e876d75ca1d59c8
Connor Crowe <connor.haz.crowe@gmail.com>
2025-08-14 11:31:21 -0400

    Add sql init that creates staging, data (prod) and meta schemas

---
+++ db/sql/init.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS staging;
+CREATE SCHEMA IF NOT EXISTS data;
+CREATE SCHEMA IF NOT EXISTS meta;
+
+CREATE TABLE IF NOT EXISTS meta.datasets (
+    dataset_name text PRIMARY KEY,
+    source_url text,
+    last_ingested timestamptz,
+    last_loaded timestamptz
+);
