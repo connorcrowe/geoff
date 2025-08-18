02e0f95d23c7f95b9e36efbe91e4a26e3f6512ea
Connor Crowe <connor.haz.crowe@gmail.com>
2025-08-18 16:22:21 -0400

    Modify infrastructure type property of bike lanes table

--- db/etl/transform/bike_lanes.sql
+++ db/etl/transform/bike_lanes.sql
@@ -15,7 +15,7 @@ SELECT
     NULLIF(owner, '')::text AS owner,
     NULLIF(dir_loworder, '')::text AS dir_loworder,
     NULLIF(infra_loworder, '')::text AS infra_loworder,
-    NULLIF(infra_highorder, '')::text AS infra_highorder,
+    NULLIF(infra_highorder, '')::text AS lane_type,
     NULLIF(converted, '')::text AS converted,
     ST_SetSRID(ST_GeomFromGeoJSON(geometry), 4326) AS geometry
 FROM staging.bike_lanes_raw;
