53c9b14fa74b5120654eef7ceff3fe528e65bf62
Connor Crowe <connor.haz.crowe@gmail.com>
2025-10-09 12:35:49 -0400

    Add pgvector to db to store vector embeddings. Create schema_embeddings table and example_embeddings table at db creation. Add populate_schema_embeddings, which will create an embedding per table row, taking descriptions from sql metatdata comments

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -10,3 +10,25 @@ CREATE TABLE IF NOT EXISTS meta.datasets (
     last_ingested timestamptz,
     last_loaded timestamptz
 );
+
+-- Database Schemas
+CREATE TABLE IF NOT EXISTS meta.schema_embeddings (
+    id SERIAL PRIMARY KEY,
+    table_name TEXT NOT NULL,
+    column_name TEXT,
+    col_type TEXT,
+    description TEXT,
+    embedding VECTOR(1536)
+);
+
+ALTER TABLE meta.schema_embeddings
+ADD CONSTRAINT unique_table_column UNIQUE(table_name, column_name);
+
+-- Example Queries
+CREATE TABLE IF NOT EXISTS meta.example_embeddings (
+    id SERIAL PRIMARY KEY,
+    tables TEXT[],
+    user_query TEXT NOT NULL,
+    plan JSONB,
+    embedding VECTOR(1536)
+);
